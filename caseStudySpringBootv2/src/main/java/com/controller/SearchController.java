7a23f090ec24b5334f4fea30fcdd643c360c2b3d
Kyle Conway <Kojacked815@gmail.com>
2021-10-13 11:30:59 -0500

    Implement Product Service Layer in Search Controller

--- caseStudySpringBootv2/src/main/java/com/controller/SearchController.java
+++ caseStudySpringBootv2/src/main/java/com/controller/SearchController.java
@@ -1,9 +1,12 @@
 package com.controller;
 
+import com.dto.ProductDTO;
 import com.entitymodels.ProductEntity;
 import com.repository.ProductRepository;
+import com.service.ProductService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -12,17 +15,22 @@ import java.util.List;
 public class SearchController {
 
     private final ProductRepository productRepository;
+    private final ProductService productService;
 
-    public SearchController(ProductRepository productRepository){
+    public SearchController(ProductRepository productRepository, ProductService productService){
         this.productRepository = productRepository;
+        this.productService = productService;
     }
 
     @GetMapping
-    public List<ProductEntity> getProducts(@RequestParam (required = false) String name){
-        if (name == "" || name == null) {
-            return productRepository.findAll();
+    public List<ProductDTO> getProducts(@RequestParam (required = false) String name){
+        if (name.equals("") || name.equals(null)) {
+            //return productRepository.findAll();
+            return productService.findAllProducts();
         }
-        return productRepository.findAllByName(name);
+        List<ProductDTO> product = new ArrayList<ProductDTO>();
+        product.add(productService.findByName(name));
+        return product;
     }
 
 
